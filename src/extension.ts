d785958a731b4f843a88911d83d632db2def300a
Hidenobu Nagai <hidenobu.nagai.jp@gmail.com>
2026-08-01 09:35:44 +0900

    chore: remove semantic highlighting debug logs

--- src/extension.ts
+++ src/extension.ts
@@ -135,14 +135,6 @@ function createState(context: vscode.ExtensionContext): ActiveExtensionState {
 }
 
 export function activate(context: vscode.ExtensionContext): void {
-  // eslint-disable-next-line no-console
-  console.log(
-    `[inline-sql] python semanticHighlighting resolved = ${String(
-      vscode.workspace
-        .getConfiguration("editor", { languageId: "python" })
-        .get("semanticHighlighting.enabled"),
-    )}`,
-  );
   disposeActiveState();
   activeState = createState(context);
 }

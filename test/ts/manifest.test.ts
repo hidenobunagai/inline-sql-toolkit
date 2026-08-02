602dfd28be318b389c9e32c679bf8459e41cc614
Hidenobu Nagai <hidenobu.nagai.jp@gmail.com>
2026-08-02 12:44:28 +0900

    release: bump version to 0.3.8 for marker adjacency

--- test/ts/manifest.test.ts
+++ test/ts/manifest.test.ts
@@ -45,7 +45,7 @@ describe("extension manifest", () => {
       name: "inline-sql-toolkit",
       displayName: "Inline SQL Toolkit",
       publisher: "hidenobunagai",
-      version: "0.3.7",
+      version: "0.3.8",
       engines: { vscode: "^1.95.0" },
       main: "./dist/extension.js",
     });
